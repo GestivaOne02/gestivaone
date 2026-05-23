7417da6b424a787cf2bfed1b1ece3d8b1a7db506
salascostore <randymendozasalas42@gmail.com>
2026-05-22 23:16:48 -0500

    style: integrate custom Google Font Elms Sans globally

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,7 +8,7 @@ export default {
   theme: {
     extend: {
       fontFamily: {
-        sans: ['Inter', 'system-ui', 'sans-serif'],
+        sans: ['Elms Sans', 'Inter', 'system-ui', 'sans-serif'],
       },
       colors: {
         brand: {
